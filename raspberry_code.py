14112033ac279318a5c86af9a7874cb82d0be9e5
Mou97 <m.taguiouine@esi-sba.dz>
2019-07-16 18:31:21 +0100

    raspberry pi's code version 1

--- raspberry_code.py
+++ raspberry_code.py
@@ -1,29 +1,47 @@
+import paho.mqtt.publish as publish
+import paho.mqtt.client as mqtt
+import time
+import json
 
-import paho.mqtt.client as client
-# connect to the mqtt client
-mqtt = client(
-"mqtt://postman.cloudmqtt.com", 
-    port=12313, 
-    user="wdvebxdg", 
-    password="QMlVLEusK9yU", 
-    ssl=True
-)
-# reading class
-class Reading(object):
-    sensor_1 = 0
-    sensor_2 = 0
-    sensor_3 = 0
+mqtt_server = "postman.cloudmqtt.com"
+topic = "sensors/sensor"
 
-def reading_value( value1 , value2 , value3 ):
-    value = Reading()
-    value.sensor_1 = value1
-    value.sensor_2 = value2
-    value.sensor_3 = value3
-    return value
+def on_connect(client , userdata , flags , rc):
+    print("Connected with rc : "+ str(rc))
+    # client.publish(topic,"first connection")
 
-#example for each reaing
-sensor_value = reading_value(15, 70 , 55 )
 
-# sensor_value is the variable which contains a single reading
+def on_publish(mosq , obj , mid):
+    print("message sent with success")
+    
+# mqtt connection
+client = mqtt.Client()
 
-mqtt.publish('sensors/sensor', str(sensor_value).encode())
+# set username and password
+client.username_pw_set("wdvebxdg","QMlVLEusK9yU")
+
+client.on_publish = on_publish
+client.on_connect = on_connect
+
+client.connect(mqtt_server,port=12313,keepalive=60)
+
+client.loop_start()
+
+# fill the variables with results of the sensors
+value1 = 100 #sensor 1
+value2 = 66 #sensor 2
+value3 = 78 #sensor 3
+
+while True : 
+    # structure the data
+    msg = {
+        "sensor1" : value1,
+        "sensor2" : value2,
+        "sensor3" : value3
+    }
+    # convert the data to JSON
+    data = json.dumps(msg)
+
+    print(str(data))
+    client.publish(topic,str(data))
+    time.sleep(1*60)

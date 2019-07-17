5c4170414156292e2ab2b668df5133538227551f
Mou97 <m.taguiouine@esi-sba.dz>
2019-07-17 01:36:55 +0100

    improve api

--- controlers/getData.js
+++ controlers/getData.js
@@ -2,14 +2,12 @@ const TrashCan = require('../models/trashCans')
 
 module.exports = async (req, res) => {
     try {
-        const can1 = await TrashCan.findOne({ trash_id: "1" })
-        const can2 = await TrashCan.findOne({ trash_id: "2" })
-        const can3 = await TrashCan.findOne({ trash_id: "3" })
+        const can = await TrashCan.find({})
 
         let data = {
-            trash1: can1.filled,
-            trash2: can2.filled,
-            trash3: can3.filled
+            trash1: can[0].filled,
+            trash2: can[1].filled,
+            trash3: can[2].filled
         }
         res.send(data)
 

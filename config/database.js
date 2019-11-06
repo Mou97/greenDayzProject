6f2b9eadb45ea09baf97fde25b13ec68709c0eb7
Taguiouine Mounir <37557533+Mou97@users.noreply.github.com>
2019-11-06 19:44:53 +0100

    update db.config

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 module.exports = {
-    db: "mongodb://laelae:brah123@ds149947.mlab.com:49947/raspberry",
+    db: "mongodb://USERNAME:PASS@ds149947.mlab.com:49947/raspberry",
     mqtt: 'mqtt://postman.cloudmqtt.com'
 }
 
